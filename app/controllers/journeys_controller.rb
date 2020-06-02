f3b57d3002959fec802a91fbd37f5db16db26714
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-06-02 11:18:16 +0100

    add nested fields and nested form

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -5,10 +5,17 @@ class JourneysController < ApplicationController
     
     def new
     end
-    
+
     def show
     end
   
     def destroy
+    end
+
+    private
+    
+    def journey_params
+      params.require(:journey).permit(:name, :start_time, 
+      meetings_attributes: [:id, :postcode, :duration, :name, :_destroy])
     end
   end
