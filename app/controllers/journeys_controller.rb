1b4cc2c75e8dc2ce7af0aa92d37f5b77f2011b98
Etienne Mustow <etienne.o.mustow@gmail.com>
2020-06-02 12:02:56 +0100

    add crud details

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,21 +1,52 @@
 class JourneysController < ApplicationController
+  before_action :set_journey, only: [:edit, :update, :destroy]
+  
     def index
       @journeys = Journey.all
     end
     
     def new
     end
-
-    def show
+    
+    def create
+      @journey = Journey.new(journey_params)
+        respond_to do |format|
+        if @journey.save
+          format.html { redirect_to edit_journey_path(@journey) }
+          flash[:notice] = 'Journey created'
+        else
+          format.html { render :new }
+          flash[:alert] = @journey.errors.full_messages.to_sentence
+        end
+      end
     end
-  
+
+    def update
+      respond_to do |format|
+        if @journey.update_attributes(journey_params)
+          format.html { redirect_to edit_journey_path(@journey) }
+          flash[:notice] = 'Journey updated'
+        else
+          format.html { render :edit }
+          flash[:alert] = @journey.errors.full_messages.to_sentence
+        end
+      end
+    end
+
     def destroy
+      if @journey.destroy
+        redirect_to journeys_url
+      end
     end
 
     private
+
+    def set_journey
+      @journey = Journey.find(params[:id])
+    end
     
     def journey_params
-      params.require(:journey).permit(:name, :start_time, 
+      params.require(:journey).permit(:name, :start_time, :start_date,
       meetings_attributes: [:id, :postcode, :duration, :name, :_destroy])
     end
   end
